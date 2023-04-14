b25f96c222b734307771085d9b7597e5b2635d0f
suleman81 <sunnybajwa904@gmail.com>
2023-04-15 03:37:12 +0500

    Commit

--- app/src/main/java/com/moutamid/backgroundcamerauploader/MainActivity.java
+++ app/src/main/java/com/moutamid/backgroundcamerauploader/MainActivity.java
@@ -6,11 +6,16 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.app.ActivityManager;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.Toast;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,36 +26,66 @@ public class MainActivity extends AppCompatActivity {
         Constants.checkApp(this);
 
         Button btn = findViewById(R.id.start);
+        if (isServiceRunning()){
+            btn.setText("Stop Service");
+        }
 
         btn.setOnClickListener(v -> {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-                if (
-                        ContextCompat.checkSelfPermission(this, Manifest.permission.READ_MEDIA_IMAGES) == PackageManager.PERMISSION_GRANTED &&
-                        ContextCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS) == PackageManager.PERMISSION_GRANTED
-                ) {
-                    Intent intent = new Intent(this, PhotoMonitorService.class);
-                    startService(intent);
-                } else {
-                    shouldShowRequestPermissionRationale(Manifest.permission.READ_MEDIA_IMAGES);
-                    shouldShowRequestPermissionRationale(Manifest.permission.POST_NOTIFICATIONS);
-                    ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_MEDIA_IMAGES, Manifest.permission.POST_NOTIFICATIONS}, 1);
-                }
+            if (isServiceRunning()){
+                btn.setText("Start Service");
+                Intent intent = new Intent(this, PhotoMonitorService.class);
+                stopService(intent);
+                Toast.makeText(this, "Service Stopped", Toast.LENGTH_SHORT).show();
             } else {
-                if (
-                        ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED &&
-                        ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED
-                ) {
-                    Intent intent = new Intent(this, PhotoMonitorService.class);
-                    startService(intent);
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                    if (
+                            ContextCompat.checkSelfPermission(this, Manifest.permission.READ_MEDIA_IMAGES) == PackageManager.PERMISSION_GRANTED &&
+                                    ContextCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS) == PackageManager.PERMISSION_GRANTED
+                    ) {
+                        Toast.makeText(this, "Service Started", Toast.LENGTH_SHORT).show();
+                        btn.setText("Stop Service");
+                        Intent intent = new Intent(this, PhotoMonitorService.class);
+                        startService(intent);
+                    } else {
+                        shouldShowRequestPermissionRationale(Manifest.permission.READ_MEDIA_IMAGES);
+                        shouldShowRequestPermissionRationale(Manifest.permission.POST_NOTIFICATIONS);
+                        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_MEDIA_IMAGES, Manifest.permission.POST_NOTIFICATIONS}, 1);
+                    }
                 } else {
-                    shouldShowRequestPermissionRationale(Manifest.permission.READ_EXTERNAL_STORAGE);
-                    shouldShowRequestPermissionRationale(Manifest.permission.WRITE_EXTERNAL_STORAGE);
-                    ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+                    if (
+                            ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED &&
+                                    ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED
+                    ) {
+                        Toast.makeText(this, "Service Started", Toast.LENGTH_SHORT).show();
+                        btn.setText("Stop Service");
+                        Intent intent = new Intent(this, PhotoMonitorService.class);
+                        startService(intent);
+                    } else {
+                        shouldShowRequestPermissionRationale(Manifest.permission.READ_EXTERNAL_STORAGE);
+                        shouldShowRequestPermissionRationale(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+                        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+                    }
                 }
             }
-
         });
 
+    }
+
+    public boolean isServiceRunning() {
+        boolean s = false;
+        ActivityManager activityManager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+
+        List<ActivityManager.RunningServiceInfo> runningServices = activityManager.getRunningServices(Integer.MAX_VALUE);
+
+        for (ActivityManager.RunningServiceInfo runningService : runningServices) {
+            if (PhotoMonitorService.class.getName().equals(runningService.service.getClassName())) {
+                // The service is running
+                // Do something here
+                s = true;
+                break;
+            }
+        }
+        return s;
     }
 
     @Override
